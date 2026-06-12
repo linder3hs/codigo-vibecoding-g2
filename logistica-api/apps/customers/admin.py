f925b4578be3e441f3ffd1a5d6e7e19b1d46a1b7
linder3hs <linderhassinger00@gmail.com>
2026-06-11 20:34:05 -0500

    feat: add gunicorn

--- logistica-api/apps/customers/admin.py
+++ logistica-api/apps/customers/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Customer
 
 
+# esta linea permite que Customer aparezca en el Django Admin
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['name', 'customer_type', 'email', 'city', 'is_active']
+    list_display = ['name', 'customer_type', 'email', 'city', 'tax_id', 'is_active']
     search_fields = ['name', 'email', 'tax_id']
     list_filter = ['is_active', 'customer_type', 'country', 'city']
